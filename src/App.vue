1602fc15c5d167bcb1558460a8a3ae66a5506685
sergio jara <sergiojosel.j@gmail.com>
2022-05-11 21:17:00 -0500

    mostrando tablas en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,148 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png" />  -->
+
+    <MostrarTabla
+      :titulo="datos1.titulo"
+      :datos="datos1.datos"
+      :columnas="datos1.columnas"
+      :fondo="datos1.fondo"
+      :color-titulo="datos1.colorTexto"
+    >
+    </MostrarTabla>
+      <MostrarTabla
+      :titulo="datos2.titulo"
+      :datos="datos2.datos"
+      :columnas="datos2.columnas"
+      :fondo="datos2.fondo"
+      :color-titulo="datos2.colorTexto"
+    >
+    </MostrarTabla>
+
+      <MostrarTabla
+      :titulo="datos3.titulo"
+      :datos="datos3.datos"
+      :columnas="datos3.columnas"
+      :fondo="datos3.fondo"
+      :color-titulo="datos3.colorTexto"
+    >
+    </MostrarTabla>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+
+import MostrarDatos from "./components/MostrarTablaDatos.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    MostrarDatos
+  },
+  data() {
+    return {
+      datos1: {
+        columnas: ["ID", "NOMBRE", "SUELDO"],
+        titulo: "Planilla Geincos SAC",
+        datos: [
+          {
+            id: 1,
+
+            nombre: "Sergio Jara Mendoza",
+
+            sueldo: 2500,
+          },
+          {
+            id: 2,
+
+            nombre: "Joselo Miranda",
+
+            sueldo: 1800,
+          },
+          {
+            id: 3,
+
+            nombre: "Ricardo Sanchez",
+
+            sueldo: 1500,
+          },
+        ],
+        fondo: {
+          "table table-dark": true,
+        },
+        colorTexto: {
+          "text-primary": true,
+        },
+      },
+
+      datos2: {
+        columnas: ["ID", "CURSO", "PRECIO"],
+        titulo: "CURSOS",
+        datos: [
+          {
+            id: 1,
+
+            curso: "LARAVEL 8",
+
+            precio: 200,
+          },
+          {
+            id: 2,
+
+            curso: "VUE JS",
+
+            precio: 250,
+          },
+          {
+            id: 3,
+
+            curso: "REACT JS",
+
+            precio: 300,
+          },
+        ],
+
+        fondo: {
+          table: true,
+        },
+        colorTexto: {
+          "text-warning": true,
+        },
+      },
+      datos3: {
+        columnas: ["ID", "AREAS"],
+        titulo: "AREAS",
+        datos: [
+          {
+            id: 1,
+
+            area: "sistemas",
+          },
+          {
+            id: 2,
+
+            area: "finanzas",
+          },
+          {
+            id: 3,
+
+            area: "logistica",
+          },
+        ],
+        fondo: {
+          "table table-striped": true,
+        },
+        colorTexto: {
+          "text-primary": true,
+        },
+      },
+    };
+  },
+};
 </script>
+  
 
 <style>
 #app {
---
+++ src/components/MostrarTablaDatos.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1 :class="colorTitulo">{{ titulo }}</h1>
+    <table :class="fondo">
+    <tr>
+        <th v-for="columna  in columnas"  :key="columna.id" scope="col">{{ columna }}</th>
+    </tr>
+
+     <tr v-for="dato  in datos" :key="dato.id">
+        <td v-for="info  in dato" :key="info.id">{{ info }}</td>
+     </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MostrarTabla",
+ 
+
+   props: {
+    titulo: String,
+    datos: Array,
+    columnas:Array,
+    fondo: Object,
+    colorTitulo: Object
+    }
+
+}
+</script>
+
+<style lang="scss" >
+
+</style>
